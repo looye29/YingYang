bf71e0359d652abd67e8409e663cb60bcfab5ceb
looye29 <info@looyestein.com>
2023-01-03 16:54:16 -0500

    update

--- pentest/Screenshot/README.md
+++
@@ -1,1 +0,0 @@
-# This is Screeshot you can see it
--- templates/passive/web/Insecure_SSL_TLS_Connection_CN_Mismatch.sh
+++
@@ -1,9 +0,0 @@
-AUTHOR='@xer0dayz'
-VULN_NAME='Insecure SSL TLS Connection CN Mismatch'
-FILENAME="$LOOT_DIR/web/curldebug-$TARGET.txt"
-MATCH='failed to verify the legitimacy of the server'
-SEVERITY='P3 - MEDIUM'
-GREP_OPTIONS='-i'
-SEARCH='positive'
-SECONDARY_COMMANDS=''
-URI="/"
